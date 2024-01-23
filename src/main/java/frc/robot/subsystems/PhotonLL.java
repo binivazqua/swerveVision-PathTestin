04ec36972e8f6c2c03910a4fe81ce0073d0e912c
binivazqua <binizavazquez@gmail.com>
2024-01-23 16:22:23 -0600

    first one

---
+++ src/main/java/frc/robot/subsystems/PhotonLL.java
@@ -0,0 +1,56 @@
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PhotonLL extends SubsystemBase {
+  PhotonCamera camera = new PhotonCamera("camera1");
+
+  private double yaw;
+
+  private double pitch;
+
+  private double area;
+
+  private double skew;
+
+
+      
+    
+  /** Creates a new ExampleSubsystem. */
+  public PhotonLL() {
+   
+
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    var result = camera.getLatestResult();
+    if (result.hasTargets()){
+      var target = result.getBestTarget();
+  
+      // GET DATA:
+      yaw = target.getYaw();
+      pitch = target.getPitch();
+      area = target.getArea();
+      skew = target.getSkew();
+
+      SmartDashboard.putNumber("Yaw", yaw);
+      SmartDashboard.putNumber("Pitch", yaw);
+      SmartDashboard.putNumber("Area", yaw);
+
+    
+    }
+
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
+
